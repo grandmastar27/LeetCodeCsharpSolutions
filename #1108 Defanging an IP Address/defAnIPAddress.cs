39219acc8dc3fcbefa7e56055bb08dd82f76cdd6
grandmastar27 <grandmastar27@gmail.com>
2020-05-16 10:46:08 +0200

    Create defAnIPAddress.cs

---
+++ #1108 Defanging an IP Address/defAnIPAddress.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public string DefangIPaddr(string address) {
+        string newIp = "";
+        for (int i = 0; i < address.Length; i++)
+        {
+            newIp += address[i].ToString().Replace(".", "[.]"); 
+        }
+        return newIp;
+    }
+}
