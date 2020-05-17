8a0cf4003868b4fedb7390c74311c404c2533c83
grandmastar27 <grandmastar27@gmail.com>
2020-05-17 12:59:58 +0200

    Update powerOfTwo.cs

--- #231 Power of Two/powerOfTwo.cs
+++ #231 Power of Two/powerOfTwo.cs
@@ -1,13 +1,13 @@
 if (n.ToString() == "")
-                return false;
-            else
-            {
-                for (int i = 0; i <= 99; i++)
-                {
-                    Console.WriteLine($"{Math.Pow(2, i)}");
-                    double pow = Math.Pow(2, i);
-                    if (pow == n)
-                        return true;
-                }
-                return false;
-            }
+    return false;
+else
+{
+    for (int i = 0; i <= 99; i++)
+    {
+        Console.WriteLine($"{Math.Pow(2, i)}");
+        double pow = Math.Pow(2, i);
+        if (pow == n)
+            return true;
+    }
+    return false;
+}
