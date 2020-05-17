13931d7e7f13bad2074d327d52763a9fbd3ce359
grandmastar27 <grandmastar27@gmail.com>
2020-05-17 18:11:00 +0200

    Create moveZeroes.cs

---
+++ #283 Move Zeroes/moveZeroes.cs
@@ -0,0 +1,19 @@
+List<int> cleaned = new List<int>();
+int zeros = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] != 0)
+        cleaned.Add(nums[i]);
+    if (nums[i] == 0)
+        zeros++;
+}
+int[] newArr = cleaned.ToArray();
+for (int i = 0; i < zeros; i++)
+{
+    newArr = newArr.Concat(new[] { 0 }).ToArray();
+}
+Array.Clear(nums, 0, nums.Length);
+for (int i = 0; i < newArr.Length; i++)
+{
+    nums[i] = newArr[i];
+}
